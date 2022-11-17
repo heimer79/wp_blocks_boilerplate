8e7b3bf113f0282e54fb37cbe94544134d83ba01
Heimer Martinez <hmartinez@cafetosoftware.com>
2022-11-16 21:29:44 -0500

    Adding options int he Block toolbar using Block controls

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '8392b4ae9a8e3796f251');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '9badc82cb9095c3d04b2');
