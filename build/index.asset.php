0fe66388f419d774fb0bda9004e3504a306d6f8d
Heimer Martinez <hmartinez@cafetosoftware.com>
2022-11-17 18:06:57 -0500

    Using the withColors Higher Order Component

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '31da00786c191e040f71');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '1432d2628f7000f5035a');
